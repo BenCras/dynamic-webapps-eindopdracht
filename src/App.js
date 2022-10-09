8b7f31668d35a7fed98851e5707f5463ade5bcdd
BenCras <r0879936@student.thomasmore.be>
2022-10-09 14:32:25 +0200

    first commit/push

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import {} from "./data/Data";
+import {Tabs, TabList, Tab, TabPanel} from "react-tabs";
+import 'react-tabs/style/react-tabs.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Tabs>
+        <TabList>
+          <Tab>start</Tab>
+        </TabList>
+        <TabPanel>
+          <h1>hello world</h1>
+        </TabPanel>
+      </Tabs>
   );
 }
 
